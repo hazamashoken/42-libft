208f8171f03d138fd778d29cb44d4bf503c738ad
tliangso <earth78203@gmail.com>
2022-07-04 21:25:33 +0800

    atoi2

--- libft/srcs/ft_atoi.c
+++ libft/srcs/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: tliangso <earth78203@gmail.co>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 15:18:20 by tliangso          #+#    #+#             */
-/*   Updated: 2022/07/04 21:17:13 by tliangso         ###   ########.fr       */
+/*   Updated: 2022/07/04 21:21:19 by tliangso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ char	check_neg(int neg)
 int	check_return(int neg)
 {
 	if (neg == 1)
-		return (0);
-	else
 		return (-1);
+	else
+		return (0);
 }
 
 int ft_atoi(const char *nptr)
@@ -52,8 +52,7 @@ int ft_atoi(const char *nptr)
 	while (*(nptr + i) >= '0' && *(nptr + i) <= '9')
 	{
 		if ((res > cutoff) || (res == cutoff && (*(nptr + i) > cutlim)))
-			if (neg == -1)
-				return (check_return(neg));
+			return (check_return(neg));
 		res = (res * 10) + (*(nptr + i++) - '0');
 	}
 	return (res * neg);
