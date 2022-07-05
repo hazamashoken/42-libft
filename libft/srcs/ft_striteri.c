6faaf44ab69c6feccbaab2060f54cdcf8ecc2a0c
tliangso <earth78203@gmail.com>
2022-07-05 20:18:49 +0800

    ft_striteri

--- libft/srcs/ft_striteri.c
+++ libft/srcs/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: tliangso <earth78203@gmail.co>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 20:15:25 by tliangso          #+#    #+#             */
-/*   Updated: 2022/07/05 20:17:59 by tliangso         ###   ########.fr       */
+/*   Updated: 2022/07/05 20:18:46 by tliangso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ void ft_striteri(char *s, void (*f)(unsigned int, char *))
 
 	i = 0;
 	if (!s)
-		return (NULL);
+		return ;
 	str = malloc((ft_strlen(s) + 1) * sizeof(char));
 	if (!str)
-		return (NULL);
+		return ;
 	while (*(s + i))
 	{
 		(*f)(i, (s + i));
