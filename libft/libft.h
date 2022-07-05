778a9eac73894b6bf3d83ad01f6db2d6c8a49c1b
tliangso <earth78203@gmail.com>
2022-07-05 20:23:49 +0800

    ft_putendl_fd

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tliangso <earth78203@gmail.co>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 16:43:18 by tliangso          #+#    #+#             */
-/*   Updated: 2022/07/05 20:16:22 by tliangso         ###   ########.fr       */
+/*   Updated: 2022/07/05 20:21:24 by tliangso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,5 +63,6 @@ void ft_swap(int *a, int *b);
 int ft_abs(int n);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void ft_striteri(char *s, void (*f)(unsigned int, char *));
+void ft_putendl_fd(char *s, int fd);
 
 #endif
