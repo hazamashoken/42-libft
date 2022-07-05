a7ef68657a371aaa440f79b1e1adbb36f751bdd4
tliangso <earth78203@gmail.com>
2022-07-05 17:01:05 +0800

    makefile libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tliangso <earth78203@gmail.co>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 16:43:18 by tliangso          #+#    #+#             */
-/*   Updated: 2022/07/05 14:23:45 by tliangso         ###   ########.fr       */
+/*   Updated: 2022/07/05 15:27:11 by tliangso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,7 @@ char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strtrim(char const *s1, char const *set);
 char **ft_split(char const *s, char c);
+void ft_putchar(char c);
+char *ft_itoa(int n);
 
 #endif
