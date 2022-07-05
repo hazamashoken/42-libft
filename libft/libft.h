7ff5206c607ebca976e8799c77b48e16d2dedf55
tliangso <earth78203@gmail.com>
2022-07-05 13:31:19 +0800

    strtrim

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: tliangso <earth78203@gmail.co>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 16:43:18 by tliangso          #+#    #+#             */
-/*   Updated: 2022/07/05 13:01:17 by tliangso         ###   ########.fr       */
+/*   Updated: 2022/07/05 13:30:15 by tliangso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,4 +47,5 @@ unsigned int ft_strlcpy(char *dest, char *src, unsigned int size);
 void *ft_calloc(size_t nmemb, size_t size);
 char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strjoin(char const *s1, char const *s2);
+char *ft_strtrim(char const *s1, char const *set);
 #endif
---
+++ libft/srcs/ft_strtrim.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tliangso <earth78203@gmail.co>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 13:08:04 by tliangso          #+#    #+#             */
+/*   Updated: 2022/07/05 13:30:02 by tliangso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	check_charset(char c, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (*(set + i))
+		if (*(set + i) == c)
+			return (1);
+	return (0);
+}
+
+size_t check_front(char const *s1, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (check_charset(*(s1 + i), set))
+		i++;
+	return (i);
+}
+
+size_t check_back(char const *s1, char const *set)
+{
+	size_t	i;
+
+	i = strlen(s1) - 1;
+	while (check_charset(*(s1 + i), set))
+		i--;
+	return (i);
+}
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	start = check_font(s1, set);
+	end = check_back(s1, set);
+	str = malloc (end - start + 1);
+	if (!str)
+		return (NULL);
+	while (i < (end - start))
+	{
+		*(str + i) = *(s1 + start + i);
+		i++;
+	}
+	*(str + i) = '\0';
+	return (str);
+}
