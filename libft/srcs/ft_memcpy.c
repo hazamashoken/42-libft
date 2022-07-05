449840258bdede6c078cc1fff156d08475fef1e0
tliangso <earth78203@gmail.com>
2022-07-05 11:40:56 +0800

    crash memcpy

--- libft/srcs/ft_memcpy.c
+++ libft/srcs/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tliangso <earth78203@gmail.co>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 17:35:37 by tliangso          #+#    #+#             */
-/*   Updated: 2022/07/05 11:38:31 by tliangso         ###   ########.fr       */
+/*   Updated: 2022/07/05 11:40:46 by tliangso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t i;
 
 	i = 0;
+	if (!dest && !src && n > 0)
+		return (NULL);
 	while (i < n)
 	{
 		*(char *)(dest + i) = *(char *)(src + i);
